5d3a2fd74daa257a3d39d3221564c52507cdc5bb
Samuel Douglas Martins Marques <samuel30douglas@gmail.com>
2023-10-03 14:15:39 -0300

    Atualizações

--- src/pages/inscrições/js/scipt.js
+++ src/pages/inscrições/js/scipt.js
@@ -1,28 +1,32 @@
 class Validator{
     constructor(){
-        this.validations = ['data-min-length',
-        ]
+        this.validations = ['data-min-length',]
     }
     validate(form){
         let inputs = form.getElementsByTagName('input');
         let inputsAsrray = [...inputs];
-
         inputsAsrray.forEach(function(input){
             for(let i = 0; this.validations.length > i; i++){
                 if(input.getAttribute(this.validations[i]) != null){
-                    let method = this.validations[i].replace('data','').replace('-', '');
+                    let method = this.validations[i].replace('data-','').replace('-', '');
                     let value = input.getAttribute(this.validations[i]);
                     this[method](input, value); 
                 }
             }
         }, this);
     }
+    minlenght(input, minValue){
+        console.log(input)
+        console.log(minValue)
+        // let inputLength = input.value.length;
+        // let errorMessage = `O campo precisa ter pelo o menos ${minValue} caracteres`;
+        // if(inputLength < minValue){
+        //     console.log(errorMessage);
+        // }
+    }
 }
 
-minLenght(input, minValue);{
-    console.log(input);
-    console.log(minValue);
-}
+
 
 let form = document.getElementById("register-form")
 let submit = document.getElementById("btn-submit")
