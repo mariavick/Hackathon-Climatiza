22baea97c0be30a39207150b3da7600ce4cb69bf
Maria Vitória <mariavivibibi120@gmail.com>
2023-10-11 17:51:28 -0300

    retirando cometérios do código

--- src/pages/fundadores/js/script.js
+++ src/pages/fundadores/js/script.js
@@ -1,20 +1,12 @@
-// Select button
-let buttons = document.querySelectorAll(".read_button"); // Adicione a class ".read_button" nos buttons
-
+let buttons = document.querySelectorAll(".read_button");
 buttons.forEach((button) => {
     button.addEventListener("click", function () {
-        // Select card
-        let card = button.parentElement; //o parend element vai pegar quem é o elemento pai do botão, ou seja, o card
-
-        // Add/Remove Class Active
+        let card = button.parentElement;
         card.classList.toggle("active");
-
         if (card.classList.contains("active")) {
             // Change button text if has class active
             return (button.textContent = "Ler menos");
         }
-
-        // Change button text if hasn't class active
         button.textContent = "Ler mais";
     });
 });
